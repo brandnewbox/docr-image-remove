0d33cb2995233a44af9c39bd2b059d4d7d0d51fb
Nathan Colgate <nathancolgate@users.noreply.github.com>
2024-09-13 14:03:53 -0500

    Update remove_images.sh

--- remove_images.sh
+++ remove_images.sh
@@ -1,7 +1,12 @@
-DIGESTS=$(doctl registry repository lm $1 --format Digest,UpdatedAt,Tags | tail -n +2 | sort -rk2 | grep -v -E "$3" | awk '{print $1}' | tail -n +$(($2+1)))
+DIGESTS=$(doctl registry repository lm $1 --format Digest,UpdatedAt,Tags | tail -n +2 | sort -rk2 | grep -v -E "$4" | awk '{print $1}' | tail -n +$(($2+1)))
 for DIGEST in $DIGESTS; do
   doctl registry repository delete-manifest $1 $DIGEST --force
 done
 echo Manifest Digests removed: $DIGESTS
 
-doctl registry garbage-collection start --include-untagged-manifests --force
+# Start garbage collection if perform_gc is true
+if [ "$3" = true ]; then
+    doctl registry garbage-collection start --include-untagged-manifests --force
+else
+    echo "Garbage collection is skipped."
+fi
